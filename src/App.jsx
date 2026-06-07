ab066363a105a5e69e62666997daad8844da7c1b
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-06-07 20:15:27 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AuthPage from './pages/AuthPage';
+import Dashboard from './pages/Dashboard';
+import NotesPage from './pages/NotesPage';
+import AssignmentsPage from './pages/AssignmentsPage';
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/notes" element={<NotesPage />} />
+        <Route path="/assignments" element={<AssignmentsPage />} />
       </Routes>
     </BrowserRouter>
   );
