2ddf0d6b16fd0372ef41258d4782d2fde02bfe6e
Aakarsh Singh <singhaakarsh1506@gmail.com>
2026-06-09 20:13:43 +0530

    Cookie Fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ const pool = require("../config/db");
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
